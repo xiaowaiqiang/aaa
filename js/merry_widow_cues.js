07f54d41733b45d1c120381f122b70d0b26aedd0
zy <519374997@qq.com>
2020-11-03 09:17:37 +0800

    图片改为原始大小 增大分辨率

--- js/merry_widow_cues.js
+++ js/merry_widow_cues.js
@@ -3,12 +3,12 @@ $(function () {
         {
             img: "img/sh_63_ori.jpg",
             video: "video/video2.mp4",
-            poster: "img/video2_poster.png"
+            poster: "img/video2_poster_ori.jpg"
         },
         {
             img: "img/sh_01_ori.jpg",
             video: "video/video1.mp4",
-            poster: "img/video1_poster.png"
+            poster: "img/video1_poster_ori.jpg"
         }];
     $(".cues_title").click(function () {
         $(".cues_title").removeClass("active");
